4f578897ba78839e7521fb614ca9e83484186ae6
DEEKSHITA2005 <108971466+DEEKSHITA2005@users.noreply.github.com>
2023-11-02 20:24:44 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import CreatePost from './components/CreatePost';
+function App() {
+  return (
+    <div>
+      <h1>My App</h1>
+      <CreatePost /> {/* Render the CreatePost component */}
+    </div>
+  );
+}
+
+export default App;
